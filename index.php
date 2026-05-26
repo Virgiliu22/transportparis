2dad08a366e203770497fb062f83331b43eb6ccc
Zagorodniuc Virgiliu <geluzag1@gmail.com>
2026-05-26 09:59:33 +0300

    Adaug raport ziua 2 si script PHP

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$mesaj = "Ziua 2: aplicația TransportParis rulează corect în PHP.";
+?>
+
+<div class="php-message">
+    <?php echo $mesaj; ?>
+</div>
+
+<script>
+    console.log("<?php echo $mesaj; ?>");
+</script>
